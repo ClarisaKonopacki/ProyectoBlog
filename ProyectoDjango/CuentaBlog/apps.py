05834830bacb97122a18f989e01380963a829be8
¨Clarisa <clarisark@gmail.com>
2022-06-21 20:34:27 -0300

    Se agrega app CuentaBlog

---
+++ ProyectoDjango/CuentaBlog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CuentablogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CuentaBlog'
---
+++ ProyectoDjango/CuentaBlog/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from CuentaBlog import views
+
+urlpatterns = [
+    path("crear/", views.SignUpView.as_view(), name ="blog_signup"),
+    path("profile/<pk>/", views.BlogProfile.as_view(), name ="blog_profile"),
+    path("editar/<pk>/", views.BlogUpdate.as_view(), name ="blog_edit"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
